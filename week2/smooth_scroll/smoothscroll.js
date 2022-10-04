b469934da5d0aedfedd63592c8a0d13a1e3260bc
Yui0126 <85067724+Yui0126@users.noreply.github.com>
2022-10-04 22:13:30 +0900

    commit

--- week2/smooth_scroll/smoothscroll.js
+++ week2/smooth_scroll/smoothscroll.js
@@ -10,17 +10,16 @@ $('nav ul li a').click(function() {
 });
 
 $(window).on('load', function() {
+    var allLinks = $('nav ul li a');
     var posts = $('section');
     var pageTop;
-    var postPos;
     var counter = 0;
+    var prevCounter = 0;
+    var doneResizing;
     var postTops = [];
 
-    posts.each(function() {
-        postTops.push(Math.floor($(this).offset().top));
-    });
+    resetPagePosition();
 
-    console.log(postTops[3]);
 
     $(window).scroll(function() {
         pageTop = $(window).scrollTop() + 210;
@@ -32,5 +31,31 @@ $(window).on('load', function() {
             counter--;
             console.log(`scrolling up! ${counter}`);
         }
+        if (counter != prevCounter) {
+            $(allLinks).removeAttr('class');
+            $('nav ul li a').eq(counter).addClass('selected');
+            prevCounter = counter;
+        }
     });
+    $(window).on('resize', function() {
+        clearTimeout(doneResizing);
+        doneResizing = setTimeout(function() {
+            resetPagePosition();
+        }, 500);
+    });
+
+    function resetPagePosition() {
+        postTops = [];
+        posts.each(function() {
+            postTops.push(Math.floor($(this).offset().top));
+        });
+        var pagePosition = $(window).scrollTop() + 210;
+        counter = 0;
+        for (var i = 0; i < postTops.length; i++) {
+            if (pagePosition > postTops[i]) { counter++; }
+        }
+        counter--;
+        $(allLinks).removeAttr('class');
+        $('nav ul li a').eq(counter).addClass('selected');
+    };
 });
