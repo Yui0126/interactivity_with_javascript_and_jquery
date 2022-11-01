47e1f83777fcfdaf573bfbce84133e8640afa49a
Yui0126 <85067724+Yui0126@users.noreply.github.com>
2022-11-01 21:16:56 +0900

    commit

--- week2/smooth_scroll_withJS/script.js
+++ week2/smooth_scroll_withJS/script.js
@@ -1,14 +1,27 @@
-const navLinks = document.querySelectorAll('nav ul li a');
+(function() {
+    'use strict';
+    const navLinks = document.querySelectorAll('nav ul li a');
 
-navLinks.forEach(function(eachLink) {
-    eachLink.addEventListener('click', smoothScroll);
-});
+    navLinks.forEach(function(eachLink) {
+        eachLink.addEventListener('click', smoothScroll);
+    });
 
-function smoothScroll(event) {
-    event.preventDefault();
-    const targetID = event.target.getAttribute('href');
-    const targetSection = document.querySelectorAll(targetID);
-    const originalTop = Math.floor(targetSection.getBoundingClientRect().top) - 200;
-    window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
-    console.log(originalTop);
-};
+    function smoothScroll(event) {
+        event.preventDefault();
+        const targetID = event.target.getAttribute('href');
+        const targetSection = document.querySelectorAll(targetID);
+        const originalTop = Math.floor(targetSection.getBoundingClientRect().top) - 200;
+        window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+        console.log(originalTop);
+    }
+
+    window.addEventListener('load', function() {
+        const posts = document.querySelectorAll('section');
+        let postTops = [];
+        let pageTop;
+        let counter = 1;
+        let prevCounter = 1;
+        let doneResizing;
+        console.log(posts[0].getBoundingClientRect().top);
+    });
+}());
