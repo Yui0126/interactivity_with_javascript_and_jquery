3e53e455d1358beec88bb4cb5a31fe680c911426
Yui0126 <85067724+Yui0126@users.noreply.github.com>
2022-11-07 20:12:07 +0900

    COMMIT

--- week2/Scription-Site/Part-2/js/script.js
+++ week2/Scription-Site/Part-2/js/script.js
@@ -1,7 +1,70 @@
 // Smooth Scroll
+$('nav ul li a').click(function() {
+    var thisSection = $(this).attr('href');
+    var thisLink = $(this);
+    $('html').stop().animate({
+        scrollTop: $(thisSection).offset().top - 200
+    }, 600, 'easeInCirc', function() {
+        $('nav ul li a').removeAttr('class');
+        thisLink.addClass('selected');
+    });
+});
 
+$(window).on('load', function() {
+    var allLinks = $('nav ul li a');
+    var posts = $('section');
+    var pageTop;
+    var counter = 0;
+    var prevCounter = 0;
+    var doneResizing;
+    var postTops = [];
+
+    resetPagePosition();
+
+
+    $(window).scroll(function() {
+        pageTop = $(window).scrollTop() + 210;
+        console.log(pageTop);
+        if (pageTop > postTops[counter + 1]) {
+            counter++;
+            console.log(`scrolling down ${counter}`);
+        } else if (counter > 0 && pageTop < postTops[counter]) {
+            counter--;
+            console.log(`scrolling up! ${counter}`);
+        }
+        if (counter != prevCounter) {
+            $(allLinks).removeAttr('class');
+            $('nav ul li a').eq(counter).addClass('selected');
+            prevCounter = counter;
+        }
+    });
+    $(window).on('resize', function() {
+        clearTimeout(doneResizing);
+        doneResizing = setTimeout(function() {
+            resetPagePosition();
+        }, 500);
+    });
+
+    function resetPagePosition() {
+        postTops = [];
+        posts.each(function() {
+            postTops.push(Math.floor($(this).offset().top));
+        });
+        var pagePosition = $(window).scrollTop() + 210;
+        counter = 0;
+        for (var i = 0; i < postTops.length; i++) {
+            if (pagePosition > postTops[i]) { counter++; }
+        }
+        counter--;
+        $(allLinks).removeAttr('class');
+        $('nav ul li a').eq(counter).addClass('selected');
+    };
+});
 
 // Flexslider
+$(window).on('load', function() {
+    $('.flexslider').flexslider();
+});
 
 
 // Tabs
@@ -10,6 +73,4 @@
 // Content Rotator
 
 
-// Features Rotator 
-
-
+// Features Rotator
